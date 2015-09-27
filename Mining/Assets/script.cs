abd723b719d28af2c5d9fe691575cb9374d72fa9
jmanacid1 <jacksonwlipfert@gmail.com>
2015-09-27 13:16:38 -0400

    Mining part 2

---
+++ Mining/Assets/script.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+public class script : MonoBehaviour {
+	public GameObject cube;
+	public bool spawned = false; 
+	// Use this for initialization
+	void Start () {
+	
+	}
+	void spawnCube () {
+		Instantiate(cube,new Vector3(0,0,0),Quaternion.identity);  
+	} 
+	// Update is called once per frame
+	void Update () {
+		if (Time.time >3 && spawned == false){
+			spawned = true;
+			spawnCube();
+		}
+	
+	}
+}
